7d15b2b5eb9c5e0f0dbf22a92474dad7b0fea568
Nilavan <nilavan.deva@etu.imt-nord-europe.fr>
2022-10-12 09:16:12 +0200

    Create struct Date, initialiseDate and afficheDate

---
+++ TP-02-pointeurs/03-Manipuler-des-dates/main.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum {
+    Janvier = 1,
+    Fevrier, 
+    Mars,
+    Avril,
+    Mai,
+    Juin,
+    Juillet,
+    Aout,
+    Septembre,
+    Octobre,
+    Novembre,
+    Decembre
+    } Mois;
+
+typedef struct {
+    int jour;
+    Mois mois;
+    int annee;
+    } Date;
+
+void initialiseDate(Date *);
+void afficheDate(Date *);
+
+int main(){
+    Date d;
+    initialiseDate(&d); //Pourquoi a t-on ajouté un &?
+    afficheDate(&d);
+    return EXIT_SUCCESS;
+}
+
+void initialiseDate(Date *d){
+    scanf("%i%i%i", &(d->jour), &(d->mois), &(d->annee));
+}
+
+void afficheDate(Date *d){
+    printf("%i/%i/%i", d->jour, d->mois, d->annee);
+}
