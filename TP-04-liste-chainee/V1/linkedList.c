4d08148805da5a74ca7c2e34babdb232b54b07d9
Nilavan <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-07 17:55:52 +0100

    Report of the modification from link-chainee.c

--- TP-04-liste-chainee/V1/linkedList.c
+++ TP-04-liste-chainee/V1/linkedList.c
@@ -52,17 +52,23 @@ void afficheListe_r(Liste l) {
 // Détruit tous les éléments de la liste l
 // version itérative
 void detruire_i(Liste l) {
+	Liste suivant;
 	while(!estVide(l)){
 		detruireElement(l->val);
-		l = l->suiv;
+		suivant = l->suiv;
+		free(l);
+		l = suivant;
 	}
 }
 
 // version récursive
 void detruire_r(Liste l) {
+	Liste suivant;
 	if(!estVide(l)){
+		suivant = l->suiv;
 		detruireElement(l->val);
-		detruire_r(l->suiv);
+		free(l);
+		detruire_r(suivant);
 	}
 }
 
@@ -70,12 +76,13 @@ void detruire_r(Liste l) {
 // version itérative
 Liste ajoutFin_i(Element v, Liste l) {
 	Liste head = l;
-	if(!estVide(l)){
-		while(!estVide(l->suiv)){
-			l = l->suiv;
-		}
-		l->suiv = creer(v);
+	if(estVide(l)){
+		return creer(v);
 	}
+	while(!estVide(l->suiv)){
+		l = l->suiv;
+	}
+	l->suiv = creer(v);
 	return head;
 }
 
@@ -115,11 +122,14 @@ Liste cherche_r(Element v,Liste l) {
 // version itérative
 Liste retirePremier_i(Element v, Liste l) {
 	Liste head = l;
+	Liste suivant;
 	if(estVide(l)){
 		return head;
 	}
 	if(equalsElement(v, l->val)){
-		return l->suiv;
+		suivant = l->suiv;
+		free(l);
+		return suivant;
 	}
 	while(!estVide(l->suiv) && !equalsElement(v, l->suiv->val)){
 		l = l->suiv;
@@ -127,21 +137,28 @@ Liste retirePremier_i(Element v, Liste l) {
 	if(estVide(l->suiv)){
 		return head;
 	}
-	l->suiv = l->suiv->suiv;
+	suivant = l->suiv->suiv;
+	free(l->suiv);
+	l->suiv = suivant;
 	return head;
 }
 
 
 // version recursive
 Liste retirePremier_r(Element v, Liste l) {
+	Liste suivant;
 	if(estVide(l)){
 		return l;
 	}
 	if(equalsElement(v, l->val)){
-		return l->suiv;
+		suivant = l->suiv;
+		free(l);
+		return suivant;
 	}
 	if(equalsElement(v, l->suiv->val)){
-		l->suiv = l->suiv->suiv;
+		suivant = l->suiv->suiv;
+		free(l->suiv);
+		l->suiv = suivant;
 		return l;
 	}
 	retirePremier_r(v, l->suiv);
