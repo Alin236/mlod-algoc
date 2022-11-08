2388c9cc34fb74598abb63368ddb52d015ab27d3
Nilavan <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-08 14:30:36 +0100

    Create mySpotify.c

---
+++ TP-04-liste-chainee/V2/mySpotify.c
@@ -0,0 +1,15 @@
+// clang -W -Wall -std=c99 linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
+
+// clang -W -Wall -Wno-unused-parameter -std=c99 linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
+
+// valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all --show-reachable=no ./mySpotify
+
+#include "../V1/linkedList.h"
+#include <stdlib.h>
+
+
+
+int main(){
+
+    return EXIT_SUCCESS;
+}
