d631e8f17efa48da5a7e654bab04acf1e36fb85d
Nilavan <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-08 16:25:21 +0100

    Get all lines and seperate each column

--- TP-04-liste-chainee/V2/mySpotify.c
+++ TP-04-liste-chainee/V2/mySpotify.c
@@ -1,12 +1,13 @@
-// clang -W -Wall -std=c99 linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
+// clang -W -Wall linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
 
-// clang -W -Wall -Wno-unused-parameter -std=c99 linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
+// clang -W -Wall -Wno-unused-parameter linkedListOfMusic.c ../V1/linkedList.c mySpotify.c -o mySpotify
 
 // valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all --show-reachable=no ./mySpotify
 
-#include "../V1/linkedList.h"
+//#include "linkedListOfMusic.c"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[]){
@@ -15,11 +16,30 @@ int main(int argc, char *argv[]){
 		fileName = argv[1];
 	}
 
-    FILE* f;
-    f = fopen(fileName,"r");
-    if(f==NULL) {
+    FILE* file;
+    file = fopen(fileName,"r");
+    if(file==NULL) {
         printf ("Erreur ouverture du fichier\n");
         return EXIT_FAILURE;
+    }
+
+    char nextLine[250];
+    fgets(nextLine, 250, file);
+
+    char* copyOfLine;
+    char *name, *artist, *album, *genre;
+    int discNumber, trackNumber, year;
+    //Music music;
+    while(fgets(nextLine, 255, file) != NULL){
+        copyOfLine = strdup(nextLine);
+        name = strsep(&copyOfLine, ",");
+        artist = strsep(&copyOfLine, ",");
+        album = strsep(&copyOfLine, ",");
+        genre = strsep(&copyOfLine, ",");
+        discNumber = atoi(strsep(&copyOfLine, ","));
+        trackNumber = atoi(strsep(&copyOfLine, ","));
+        year = atoi(strsep(&copyOfLine, ","));
+        printf("%s\n", name);
     }
 
     return EXIT_SUCCESS;
